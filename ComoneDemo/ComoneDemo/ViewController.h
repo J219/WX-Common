7264ba0656062cd343fa5856eb4a756a2eb9c00c
J219 <end1988@126.com>
2015-01-08 16:14:41 +0800

    common demo 工程创建

---
+++ ComoneDemo/ComoneDemo/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  ComoneDemo
+//
+//  Created by WangXun on 15/1/8.
+//  Copyright (c) 2015年 WangXun. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
